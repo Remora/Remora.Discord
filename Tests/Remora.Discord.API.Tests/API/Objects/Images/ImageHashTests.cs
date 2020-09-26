0bd61d1b25b31fb7a396f426dee37e4add98c67d
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-09-26 18:43:03 +0200

    Add tests for HasGif.

--- Tests/Remora.Discord.API.Tests/API/Objects/Images/ImageHashTests.cs
+++ Tests/Remora.Discord.API.Tests/API/Objects/Images/ImageHashTests.cs
@@ -21,12 +21,35 @@
 //
 
 using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Objects;
 using Remora.Discord.API.Tests.TestBases;
+using Xunit;
+
+#pragma warning disable CS1591, SA1600
 
 namespace Remora.Discord.API.Tests.Objects
 {
     /// <inheritdoc />
     public class ImageHashTests : ObjectTestBase<IImageHash>
     {
+        /// <summary>
+        /// Tests the <see cref="IImageHash.HasGif"/> method.
+        /// </summary>
+        public class HasGif
+        {
+            [Fact]
+            public void ReturnsTrueForAnimatedHash()
+            {
+                var hash = new ImageHash("a_1269e74af4df7417b13759eae50c83dc");
+                Assert.True(hash.HasGif);
+            }
+
+            [Fact]
+            public void ReturnsFalseForStaticHash()
+            {
+                var hash = new ImageHash("1269e74af4df7417b13759eae50c83dc");
+                Assert.False(hash.HasGif);
+            }
+        }
     }
 }
