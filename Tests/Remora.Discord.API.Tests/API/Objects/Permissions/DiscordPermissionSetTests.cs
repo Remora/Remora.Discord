9d5a70576102ed7ba37697d0ec9fca6cd4ea286d
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-09-26 19:37:45 +0200

    Add samples and tests for discord permission sets.

--- Tests/Remora.Discord.API.Tests/API/Objects/Permissions/DiscordPermissionSetTests.cs
+++ Tests/Remora.Discord.API.Tests/API/Objects/Permissions/DiscordPermissionSetTests.cs
@@ -20,13 +20,80 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System.Numerics;
 using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Objects;
 using Remora.Discord.API.Tests.TestBases;
+using Xunit;
+
+#pragma warning disable CS1591, SA1600
 
 namespace Remora.Discord.API.Tests.Objects
 {
     /// <inheritdoc />
     public class DiscordPermissionSetTests : ObjectTestBase<IDiscordPermissionSet>
     {
+        [Fact]
+        public void HasPermissionReturnsTrueForExistingPermission()
+        {
+            var permissions = new DiscordPermissionSet(BigInteger.One);
+            var permission = DiscordPermission.CreateInstantInvite;
+
+            Assert.True(permissions.HasPermission(permission));
+        }
+
+        [Fact]
+        public void HasPermissionReturnsFalseForNonExistentPermission()
+        {
+            var permissions = new DiscordPermissionSet(BigInteger.One);
+            var permission = DiscordPermission.KickMembers;
+
+            Assert.False(permissions.HasPermission(permission));
+        }
+
+        [Fact]
+        public void HasPermissionReturnsFalseForNonExistentPermissionInAnotherByte()
+        {
+            var permissions = new DiscordPermissionSet(BigInteger.One);
+            var permission = DiscordPermission.MoveMembers;
+
+            Assert.False(permissions.HasPermission(permission));
+        }
+
+        [Fact]
+        public void HasPermissionReturnsTrueForExistingTextPermission()
+        {
+            var permissions = new DiscordPermissionSet(BigInteger.One);
+            var permission = DiscordTextPermission.CreateInstantInvite;
+
+            Assert.True(permissions.HasPermission(permission));
+        }
+
+        [Fact]
+        public void HasPermissionReturnsFalseForNonExistentTextPermission()
+        {
+            var permissions = new DiscordPermissionSet(BigInteger.One);
+            var permission = DiscordTextPermission.ManageChannels;
+
+            Assert.False(permissions.HasPermission(permission));
+        }
+
+        [Fact]
+        public void HasPermissionReturnsTrueForExistingVoicePermission()
+        {
+            var permissions = new DiscordPermissionSet(BigInteger.One);
+            var permission = DiscordVoicePermission.CreateInstantInvite;
+
+            Assert.True(permissions.HasPermission(permission));
+        }
+
+        [Fact]
+        public void HasPermissionReturnsFalseForNonExistentVoicePermission()
+        {
+            var permissions = new DiscordPermissionSet(BigInteger.One);
+            var permission = DiscordVoicePermission.ManageChannels;
+
+            Assert.False(permissions.HasPermission(permission));
+        }
     }
 }
