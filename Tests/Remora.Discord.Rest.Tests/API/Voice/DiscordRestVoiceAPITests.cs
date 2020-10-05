50f3746018e5a19c02212595765199abab8c8ae7
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-10-05 12:17:44 +0200

    Add webhook REST API tests.

---
+++ Tests/Remora.Discord.Rest.Tests/API/Voice/DiscordRestVoiceAPITests.cs
@@ -0,0 +1,71 @@
+//
+//  DiscordRestVoiceAPITests.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Core;
+using Remora.Discord.Rest.API;
+using Remora.Discord.Rest.Tests.Extensions;
+using Remora.Discord.Rest.Tests.TestBases;
+using Remora.Discord.Tests;
+using RichardSzalay.MockHttp;
+using Xunit;
+
+namespace Remora.Discord.Rest.Tests.API.Voice
+{
+    /// <summary>
+    /// Tests the <see cref="DiscordRestVoiceAPI"/> class.
+    /// </summary>
+    public class DiscordRestVoiceAPITests
+    {
+        /// <summary>
+        /// Tests the <see cref="DiscordRestVoiceAPI.ListVoiceRegionsAsync"/> method.
+        /// </summary>
+        public class ListVoiceRegionsAsync : RestAPITestBase<IDiscordRestVoiceAPI>
+        {
+            /// <summary>
+            /// Tests whether the API method performs its request correctly.
+            /// </summary>
+            /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+            [Fact]
+            public async Task PerformsRequestCorrectly()
+            {
+                var api = CreateAPI
+                (
+                    b => b
+                        .Expect(HttpMethod.Get, $"{Constants.BaseURL}voice/regions")
+                        .Respond("application/json", "[]")
+                );
+
+                var result = await api.ListVoiceRegionsAsync();
+                ResultAssert.Successful(result);
+            }
+        }
+    }
+}
