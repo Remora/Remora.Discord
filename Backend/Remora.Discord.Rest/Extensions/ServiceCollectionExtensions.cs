29cadef7b6a9db2377d71350f6e6ebfee7d3e794
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-10-11 19:46:44 +0200

    Register the template API.

--- Backend/Remora.Discord.Rest/Extensions/ServiceCollectionExtensions.cs
+++ Backend/Remora.Discord.Rest/Extensions/ServiceCollectionExtensions.cs
@@ -33,6 +33,7 @@ using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Extensions;
 using Remora.Discord.Core;
 using Remora.Discord.Rest.API;
+using Remora.Discord.Rest.API.Templates;
 using Remora.Discord.Rest.Polly;
 
 namespace Remora.Discord.Rest.Extensions
@@ -131,7 +132,8 @@ namespace Remora.Discord.Rest.Extensions
                 .AddScoped<IDiscordRestInviteAPI, DiscordRestInviteAPI>()
                 .AddScoped<IDiscordRestUserAPI, DiscordRestUserAPI>()
                 .AddScoped<IDiscordRestVoiceAPI, DiscordRestVoiceAPI>()
-                .AddScoped<IDiscordRestWebhookAPI, DiscordRestWebhookAPI>();
+                .AddScoped<IDiscordRestWebhookAPI, DiscordRestWebhookAPI>()
+                .AddScoped<IDiscordRestTemplateAPI, DiscordRestTemplateAPI>();
 
             return serviceCollection;
         }
