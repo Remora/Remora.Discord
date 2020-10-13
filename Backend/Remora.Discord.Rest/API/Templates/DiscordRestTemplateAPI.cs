c9c465a8c659f6dfa434359422fc9dd6a33a91ba
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-10-13 18:54:29 +0200

    Fix namespace.

--- Backend/Remora.Discord.Rest/API/Templates/DiscordRestTemplateAPI.cs
+++ Backend/Remora.Discord.Rest/API/Templates/DiscordRestTemplateAPI.cs
@@ -34,7 +34,7 @@ using Remora.Discord.Rest.Extensions;
 using Remora.Discord.Rest.Results;
 using Remora.Discord.Rest.Utility;
 
-namespace Remora.Discord.Rest.API.Templates
+namespace Remora.Discord.Rest.API
 {
     /// <inheritdoc />
     public class DiscordRestTemplateAPI : IDiscordRestTemplateAPI
