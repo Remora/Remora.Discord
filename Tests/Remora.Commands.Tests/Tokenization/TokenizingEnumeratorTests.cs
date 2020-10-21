e8e3bcf3576d586a793689053ceb0ba00a3a6f76
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-10-21 18:26:30 +0200

    Add checking for token values.

--- Tests/Remora.Commands.Tests/Tokenization/TokenizingEnumeratorTests.cs
+++ Tests/Remora.Commands.Tests/Tokenization/TokenizingEnumeratorTests.cs
@@ -37,29 +37,38 @@ namespace Remora.Commands.Tests.Tokenization
         /// </summary>
         /// <param name="value">The value to tokenize.</param>
         /// <param name="expectedTokenTypes">The type sequence to expect.</param>
+        /// <param name="expectedTokenValues">The value sequence to expect.</param>
         [Theory]
-        [InlineData("", new TokenType[] { })]
-        [InlineData("a", new[] { Value })]
-        [InlineData("a b", new[] { Value, Value })]
-        [InlineData("-a b", new[] { ShortName, Value })]
-        [InlineData("-a=b", new[] { ShortName, Value })]
-        [InlineData("--a b", new[] { LongName, Value })]
-        [InlineData("--a=b", new[] { LongName, Value })]
-        [InlineData("-a -b", new[] { ShortName, ShortName })]
-        [InlineData("--a --b", new[] { LongName, LongName })]
-        [InlineData("--a --b c", new[] { LongName, LongName, Value })]
-        [InlineData("--b \"booga wooga\"", new[] { LongName, Value })]
-        [InlineData("--b=\"booga wooga\"", new[] { LongName, Value })]
-        [InlineData("aa bb --b=\"booga wooga\"", new[] { Value, Value, LongName, Value })]
-        public void TokenizesStringCorrectly(string value, IEnumerable<TokenType> expectedTokenTypes)
+        [InlineData("", new TokenType[] { }, new string[] { })]
+        [InlineData("a", new[] { Value }, new[] { "a" })]
+        [InlineData("a b", new[] { Value, Value }, new[] { "a", "b" })]
+        [InlineData("-a b", new[] { ShortName, Value }, new[] { "a", "b" })]
+        [InlineData("-a=b", new[] { ShortName, Value }, new[] { "a", "b" })]
+        [InlineData("--a b", new[] { LongName, Value }, new[] { "a", "b" })]
+        [InlineData("--a=b", new[] { LongName, Value }, new[] { "a", "b" })]
+        [InlineData("-a -b", new[] { ShortName, ShortName }, new[] { "a", "b" })]
+        [InlineData("--a --b", new[] { LongName, LongName }, new[] { "a", "b" })]
+        [InlineData("--a --b c", new[] { LongName, LongName, Value }, new[] { "a", "b", "c" })]
+        [InlineData("--b \"booga wooga\"", new[] { LongName, Value }, new[] { "b", "booga wooga" })]
+        [InlineData("--b=\"booga wooga\"", new[] { LongName, Value }, new[] { "b", "booga wooga" })]
+        [InlineData("aa bb --b=\"booga wooga\"", new[] { Value, Value, LongName, Value }, new[] { "aa", "bb", "b", "booga wooga" })]
+        public void TokenizesStringCorrectly
+        (
+            string value,
+            IEnumerable<TokenType> expectedTokenTypes,
+            IEnumerable<string> expectedTokenValues
+        )
         {
             var actualTokenTypes = new List<TokenType>();
+            var actualTokenValues = new List<string>();
             foreach (var token in new TokenizingEnumerator(value))
             {
                 actualTokenTypes.Add(token.Type);
+                actualTokenValues.Add(token.Value.ToString());
             }
 
             Assert.Equal(expectedTokenTypes, actualTokenTypes);
+            Assert.Equal(expectedTokenValues, actualTokenValues);
         }
     }
 }
