fa26ec7c9dddc7dc7607ab92fe62fccf857524c9
Nikey646 <nikey646@hotmail.com>
2020-11-07 23:32:26 +1100

    Added a simple Microsoft.Extensions.Hosting impl
    Created as a new project to not require the abstractions for users who
    don't want to use Microsoft.Extensions.Hosting.

---
+++ Remora.Discord.Hosting/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,51 @@
+//
+//  ServiceCollectionExtensions.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Remora.Discord.Gateway.Extensions;
+using Remora.Discord.Hosting.Services;
+
+namespace Remora.Discord.Hosting.Extensions
+{
+    /// <summary>
+    /// Defines extension methods for the <see cref="IServiceCollection"/> interface.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the required services for Remora Discord and a <see cref="IHostedService"/> implementation.
+        /// </summary>
+        /// <param name="serviceCollection">The service collection.</param>
+        /// <param name="token">A function that retrieves the bot token.</param>
+        /// <returns>The service collection, with the services added.</returns>
+        public static IServiceCollection AddDiscordService(this IServiceCollection serviceCollection, Func<string> token)
+        {
+            serviceCollection
+                .AddDiscordGateway(token)
+                .AddSingleton<IHostedService, DiscordService>();
+
+            return serviceCollection;
+        }
+    }
+}
