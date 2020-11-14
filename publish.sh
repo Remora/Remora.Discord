76d933c7d6fda46cf1a194c3296f6a77f5b4c82f
Jarl Gullberg <jarl.gullberg@gmail.com>
2020-11-14 16:48:55 +0100

    Add a small helper script for publishing to nuget.

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+declare -r LOCAL_DIR=$(realpath $(dirname $0))
+
+if [[ -d nuget ]]; then
+    rm -rf nuget
+fi
+
+dotnet pack -c Release
+pushd nuget
+    for f in *.symbols.nupkg; do 
+        dotnet nuget push --skip-duplicate -k $NUGET_AUTH_KEY $f
+    done
+popd
+
