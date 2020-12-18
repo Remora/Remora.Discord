816d368675a4e9771cce5ac786bcacbf67ac6ab7
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2020-12-18 17:23:06 +0100

    Improve publish script.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 declare -r LOCAL_DIR=$(realpath $(dirname $0))
+cd "${LOCAL_DIR}"
 
 if [[ -d nuget ]]; then
     rm -rf nuget
@@ -9,7 +10,7 @@ fi
 dotnet pack -c Release
 pushd nuget
     for f in *.symbols.nupkg; do 
-        dotnet nuget push --skip-duplicate -k $NUGET_AUTH_KEY $f
+        dotnet nuget push --skip-duplicate -k "${NUGET_AUTH_KEY}" "${f}"
     done
 popd
 
