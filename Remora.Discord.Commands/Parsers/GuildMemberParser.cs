90da7e5ae6b0936b668ff7fc628bddac69dd8e63
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2020-12-27 18:15:47 +0100

    Remove unused usings.

--- Remora.Discord.Commands/Parsers/GuildMemberParser.cs
+++ Remora.Discord.Commands/Parsers/GuildMemberParser.cs
@@ -20,7 +20,6 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Remora.Commands.Parsers;
--- Remora.Discord.Commands/Parsers/UserParser.cs
+++ Remora.Discord.Commands/Parsers/UserParser.cs
@@ -20,8 +20,6 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Remora.Commands.Parsers;
