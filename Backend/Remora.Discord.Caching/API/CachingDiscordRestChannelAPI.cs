f76d1be32de6cf9352817633706fe9279924c24d
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2020-12-27 14:22:26 +0100

    Override correct method.

--- Backend/Remora.Discord.Caching/API/CachingDiscordRestChannelAPI.cs
+++ Backend/Remora.Discord.Caching/API/CachingDiscordRestChannelAPI.cs
@@ -184,7 +184,7 @@ namespace Remora.Discord.Caching.API
             Optional<string> content = default,
             Optional<string> nonce = default,
             Optional<bool> isTTS = default,
-            Optional<Stream> file = default,
+            Optional<FileData> file = default,
             Optional<IEmbed> embed = default,
             Optional<IAllowedMentions> allowedMentions = default,
             Optional<IMessageReference> messageReference = default,
