d1f94d35b52128d8aa2d2a441d568aef8baf57e1
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2021-08-01 20:40:25 +0200

    Update versions & start implementing tests for execution events.

---
+++ Tests/Remora.Discord.Commands.Tests/Data/Events/SimpleGroup.cs
@@ -0,0 +1,49 @@
+//
+//  SimpleGroup.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System.Threading.Tasks;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Results;
+
+namespace Remora.Discord.Commands.Tests.Data.Events
+{
+    /// <summary>
+    /// A simple command group for testing execution events.
+    /// </summary>
+    public class SimpleGroup : CommandGroup
+    {
+        /// <summary>
+        /// A command that will always execute successfully.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [Command("successful")]
+        public Task<Result> Successful() => Task.FromResult(Result.FromSuccess());
+
+        /// <summary>
+        /// A command that will always execute unsuccessfully.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [Command("unsuccessful")]
+        public Task<Result> Unsuccessful() => Task.FromResult(Result.FromError(new InvalidOperationError("Oh no!")));
+    }
+}
---
+++ Tests/Remora.Discord.Commands.Tests/TestBases/InteractionResponderTestBase.cs
@@ -0,0 +1,73 @@
+//
+//  InteractionResponderTestBase.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Remora.Discord.Commands.Extensions;
+using Remora.Discord.Commands.Responders;
+
+namespace Remora.Discord.Commands.Tests.TestBases
+{
+    /// <summary>
+    /// Tests the command responder.
+    /// </summary>
+    public abstract class InteractionResponderTestBase : IDisposable
+    {
+        private readonly IServiceScope _scope;
+
+        /// <summary>
+        /// Gets the responder under test.
+        /// </summary>
+        protected InteractionResponder Responder { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InteractionResponderTestBase"/> class.
+        /// </summary>
+        public InteractionResponderTestBase()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddDiscordCommands();
+
+            // ReSharper disable once VirtualMemberCallInConstructor
+            ConfigureServices(serviceCollection);
+
+            var services = serviceCollection.BuildServiceProvider();
+
+            _scope = services.CreateScope();
+            this.Responder = _scope.ServiceProvider.GetRequiredService<InteractionResponder>();
+        }
+
+        /// <summary>
+        /// Configures additional required services.
+        /// </summary>
+        /// <param name="serviceCollection">The service collection.</param>
+        protected virtual void ConfigureServices(IServiceCollection serviceCollection)
+        {
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _scope.Dispose();
+        }
+    }
+}
