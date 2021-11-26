27f9370dee6aa701809ca83c2da5f758a16d04ec
Carl Stephens <carlst99@outlook.co.nz>
2021-11-27 10:47:18 +1300

    Add voice type implementations

---
+++ Backend/Remora.Discord.API/API/VoiceGateway/Events/VoiceHello.cs
@@ -0,0 +1,35 @@
+﻿//
+//  VoiceHello.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using JetBrains.Annotations;
+using Remora.Discord.API.Abstractions.VoiceGateway.Events;
+
+namespace Remora.Discord.API.VoiceGateway.Events;
+
+/// <inheritdoc cref="IVoiceHello" />
+[PublicAPI]
+public record VoiceHello
+(
+    int Version,
+    TimeSpan HeartbeatInterval
+) : IVoiceHello;
