3033842b706f1d56a02d65ee0d4f838f22c18422
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-01-27 18:42:14 -0500

    Add file header and constructor

--- Remora.Discord.Commands/Parsers/AttachmentParser.cs
+++ Remora.Discord.Commands/Parsers/AttachmentParser.cs
@@ -1,3 +1,25 @@
+//
+//  AttachmentParser.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 using System.Threading;
 using System.Threading.Tasks;
 using Remora.Commands.Parsers;
@@ -15,6 +37,15 @@ namespace Remora.Discord.Commands.Parsers;
 public class AttachmentParser : AbstractTypeParser<IAttachment>
 {
     private readonly ICommandContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AttachmentParser"/> class.
+    /// </summary>
+    /// <param name="context">The command context.</param>
+    public AttachmentParser(ICommandContext context)
+    {
+        _context = context;
+    }
 
     /// <inheritdoc/>
     public override async ValueTask<Result<IAttachment>> TryParseAsync(string token, CancellationToken ct = default)
