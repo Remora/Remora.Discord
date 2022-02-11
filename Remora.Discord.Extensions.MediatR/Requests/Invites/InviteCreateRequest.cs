a1b897815501b94d9540eb00d09be5fcf2e2c3d3
Foxtrek_64 <foxtrek64@users.noreply.github.com>
2022-02-10 20:56:09 -0600

    Add integration, interaction, and invite requests

---
+++ Remora.Discord.Extensions.MediatR/Requests/Invites/InviteCreateRequest.cs
@@ -0,0 +1,32 @@
+﻿//
+//  InviteCreateRequest.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using JetBrains.Annotations;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+
+namespace Remora.Discord.Extensions.MediatR.Requests.Invites
+{
+    /// <inheritdoc />
+    [PublicAPI]
+    public sealed record InviteCreateRequest<TInviteCreate>(TInviteCreate Event) : IGatewayEventRequest<TInviteCreate>
+        where TInviteCreate : IInviteCreate;
+}
