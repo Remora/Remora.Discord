212d7ecb1660232a0a0b716d12e3fff90fb2b88a
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-03-16 20:36:45 -0400

    Attributes

---
+++ Remora.Discord.Commands/Attributes/DiscordDefaultDMPermissionAttribute.cs
@@ -0,0 +1,37 @@
+//
+//  DiscordDefaultDMPermissionAttribute.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+
+namespace Remora.Discord.Commands.Attributes;
+
+/// <summary>
+/// Marks a command group as being either executable or non-executable in a DM.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class DiscordDefaultDMPermissionAttribute : Attribute
+{
+    /// <summary>
+    /// Gets a value indicating whether the command group is executable in a DM.
+    /// </summary>
+    public bool IsExecutableInDMs { get; }
+}
---
+++ Remora.Discord.Commands/Attributes/DiscordDefaultMemberPermissionsAttrribute.cs
@@ -0,0 +1,40 @@
+//
+//  DiscordDefaultMemberPermissionsAttrribute.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using Remora.Discord.API.Abstractions.Objects;
+
+namespace Remora.Discord.Commands.Attributes;
+
+/// <summary>
+/// Marks a group with a set of permissions, that is the permissions required
+/// for the user to be able to execute the commands within the group. Permissions
+/// can be overridden by moderators.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class DiscordDefaultMemberPermissionsAttrribute : Attribute
+{
+    /// <summary>
+    /// Gets the required permissions to execute the command.
+    /// </summary>
+    public DiscordPermission Permissions { get; }
+}
