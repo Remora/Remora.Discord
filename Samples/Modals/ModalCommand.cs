799a6f1e23d62c59ab7d54f6c8174db78a0afbd0
NoobyPilot <cedric.schleimer@gmail.com>
2022-04-19 17:43:18 +0200

    Adding a Sample for Modals

---
+++ Samples/Modals/ModalCommand.cs
@@ -0,0 +1,87 @@
+﻿//
+//  ModalCommand.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System.ComponentModel;
+using Remora.Commands.Attributes;
+using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.API.Objects;
+using Remora.Discord.Commands.Attributes;
+using Remora.Discord.Commands.Contexts;
+using Remora.Discord.Commands.Responders;
+using Remora.Results;
+
+namespace Remora.Discord.Samples.Modal;
+
+/// <inheritdoc />
+public class ModalCommand : CommandGroup
+{
+    private readonly IDiscordRestInteractionAPI _interactionApi;
+    private readonly InteractionContext _context;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModalCommand"/> class.
+    /// </summary>
+    /// <param name="interactionApi">Discord interaction Api.</param>
+    /// <param name="context">The Interaction's context.</param>
+    public ModalCommand(IDiscordRestInteractionAPI interactionApi, InteractionContext context)
+    {
+        _interactionApi = interactionApi;
+        _context = context;
+    }
+
+    /// <summary>
+    /// Shows a modal.
+    /// </summary>
+    /// <returns>A result, indicating if the modal was sent successfully.</returns>
+    [Command("modal")]
+    [Description("Shows a modal")]
+    [CommandType(ApplicationCommandType.ChatInput)]
+    [SuppressInteractionResponse(true)]
+    public async Task<Result> OnModal()
+    {
+        var response = new InteractionResponse(
+                                               InteractionCallbackType.Modal,
+                                               new(
+                                                   new InteractionModalCallbackData("test-modal", "Test Modal", new[]
+                                                           {
+                                                               new ActionRowComponent(new[]
+                                                               {
+                                                                   new TextInputComponent(
+                                                                    "modal-text-input",
+                                                                    TextInputStyle.Short,
+                                                                    "Short Text",
+                                                                    1,
+                                                                    32,
+                                                                    true,
+                                                                    string.Empty,
+                                                                    "Short Text here")
+                                                               })
+                                                           }
+                                                       )));
+
+        var result =
+            await _interactionApi.CreateInteractionResponseAsync(_context.ID, _context.Token, response, ct: CancellationToken);
+        return result;
+    }
+}
---
+++ Samples/Modals/ModalReceiver.cs
@@ -0,0 +1,60 @@
+﻿//
+// ModalReceiver.cs
+//
+//  Author:
+//       Jarl Gullberg <jarl.gullberg@gmail.com>
+//
+//  Copyright (c) 2017 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Objects;
+using Remora.Discord.Interactivity;
+using Remora.Results;
+
+namespace Remora.Discord.Samples.Modal;
+
+/// <inheritdoc />
+public class ModalReceiver : IModalInteractiveEntity
+{
+    private ILogger<ModalReceiver> _log;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModalReceiver"/> class.
+    /// </summary>
+    /// <param name="log">Logger used to log the received information.</param>
+    public ModalReceiver(ILogger<ModalReceiver> log)
+    {
+        _log = log;
+    }
+
+    /// <inheritdoc />
+    public Task<Result<bool>> IsInterestedAsync(ComponentType? componentType, string customID, CancellationToken ct = default)
+    {
+        return Task.FromResult(Result<bool>.FromSuccess(customID == "test-modal"));
+    }
+
+    /// <inheritdoc />
+    public Task<Result> HandleInteractionAsync(IUser user, string customID, IReadOnlyList<IPartialMessageComponent> components, CancellationToken ct = default)
+    {
+        _log.LogInformation("Received modal response");
+        var row = (PartialActionRowComponent)components[0];
+        var textInput = (PartialTextInputComponent)row.Components.Value[0];
+        _log.LogInformation("Received input: {0}", textInput.Value);
+        return Task.FromResult(Result.FromSuccess());
+    }
+}
