b170522801a49b12c426039c0d702f6456919c86
Jarl Gullberg <jarl.gullberg@gmail.com>
2022-07-26 19:10:35 +0200

    Remove unused usings.

--- Remora.Discord.Commands/Services/SlashService.cs
+++ Remora.Discord.Commands/Services/SlashService.cs
@@ -20,9 +20,7 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
