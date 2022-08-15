f5b03d06ac316b57d936a5d7295e7b8864edf2e2
Jarl Gullberg <jarl.gullberg@gmail.com>
2022-08-15 20:26:49 +0200

    Use Remora.Sdk and friends.

--- Remora.Discord.Commands/Services/ITreeNameResolver.cs
+++ Remora.Discord.Commands/Services/ITreeNameResolver.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Jarl Gullberg <jarl.gullberg@gmail.com>
 //
-//  Copyright (c) 2017 Jarl Gullberg
+//  Copyright (c) Jarl Gullberg
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
