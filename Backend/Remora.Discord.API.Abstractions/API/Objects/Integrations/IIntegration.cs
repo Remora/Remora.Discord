c86c7ea687333a8b00e6010e3dacae2602ad379c
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2022-12-25 21:05:12 +0100

    Fix optionality of several fields in IIntegration.

--- Backend/Remora.Discord.API.Abstractions/API/Objects/Integrations/IIntegration.cs
+++ Backend/Remora.Discord.API.Abstractions/API/Objects/Integrations/IIntegration.cs
@@ -56,12 +56,12 @@ public interface IIntegration : IPartialIntegration
     /// <summary>
     /// Gets a value indicating whether the integration is syncing.
     /// </summary>
-    new bool IsSyncing { get; }
+    new Optional<bool> IsSyncing { get; }
 
     /// <summary>
     /// Gets the ID of the role that this integration uses for subscribers.
     /// </summary>
-    new Snowflake RoleID { get; }
+    new Optional<Snowflake> RoleID { get; }
 
     /// <summary>
     /// Gets a value indicating whether emoticons should be synced for this integration (twitch only, currently).
@@ -71,12 +71,12 @@ public interface IIntegration : IPartialIntegration
     /// <summary>
     /// Gets the behaviour of expiring subscribers.
     /// </summary>
-    new IntegrationExpireBehaviour ExpireBehaviour { get; }
+    new Optional<IntegrationExpireBehaviour> ExpireBehaviour { get; }
 
     /// <summary>
     /// Gets the grace period (in days) before expiring subscribers.
     /// </summary>
-    new TimeSpan ExpireGracePeriod { get; }
+    new Optional<TimeSpan> ExpireGracePeriod { get; }
 
     /// <summary>
     /// Gets the user for this integration.
@@ -91,17 +91,17 @@ public interface IIntegration : IPartialIntegration
     /// <summary>
     /// Gets the time when the integration was last synced.
     /// </summary>
-    new DateTimeOffset SyncedAt { get; }
+    new Optional<DateTimeOffset> SyncedAt { get; }
 
     /// <summary>
     /// Gets the number of subscribers this integration has.
     /// </summary>
-    new int SubscriberCount { get; }
+    new Optional<int> SubscriberCount { get; }
 
     /// <summary>
     /// Gets a value indicating whether this integration has been revoked.
     /// </summary>
-    new bool IsRevoked { get; }
+    new Optional<bool> IsRevoked { get; }
 
     /// <summary>
     /// Gets the bot/OAuth2 application for Discord integrations.
