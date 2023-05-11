964af423e75b520b0efed00dc49459a5bcc86962
Jarl Gullberg <jarl.gullberg@gmail.com>
2023-05-11 21:06:19 +0200

    Update samples.

--- Samples/Interactivity/Interactions/ModalInteractions.cs
+++ Samples/Interactivity/Interactions/ModalInteractions.cs
@@ -6,6 +6,8 @@
 
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Remora.Discord.Commands.Attributes;
+using Remora.Discord.Commands.Feedback.Services;
 using Remora.Discord.Interactivity;
 using Remora.Results;
 
@@ -17,14 +19,17 @@ namespace Remora.Discord.Samples.Interactivity.Interactions;
 public class ModalInteractions : InteractionGroup
 {
     private readonly ILogger<ModalInteractions> _log;
+    private readonly FeedbackService _feedbackService;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ModalInteractions"/> class.
     /// </summary>
     /// <param name="log">The logging instance for this type.</param>
-    public ModalInteractions(ILogger<ModalInteractions> log)
+    /// <param name="feedbackService">The feedback service.</param>
+    public ModalInteractions(ILogger<ModalInteractions> log, FeedbackService feedbackService)
     {
         _log = log;
+        _feedbackService = feedbackService;
     }
 
     /// <summary>
@@ -39,5 +44,20 @@ public class ModalInteractions : InteractionGroup
         _log.LogInformation("Received input: {Input}", modalTextInput);
 
         return Task.FromResult(Result.FromSuccess());
+    }
+
+    /// <summary>
+    /// Logs submitted modal data.
+    /// </summary>
+    /// <param name="modalTextInput">The value of the modal text input component.</param>
+    /// <returns>A result which may or may not have succeeded.</returns>
+    [Modal("ephemeral-modal")]
+    [Ephemeral]
+    public async Task<Result> OnEphemeralModalSubmitAsync(string modalTextInput)
+    {
+        _log.LogInformation("Received modal response");
+        _log.LogInformation("Received input: {Input}", modalTextInput);
+
+        return (Result)await _feedbackService.SendContextualSuccessAsync(modalTextInput);
     }
 }
