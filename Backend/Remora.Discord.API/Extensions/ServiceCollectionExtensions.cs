6d28bbb9522a427fa603d91adfde2546e6fbfcaa
Jarl Gullberg <jarl.gullberg@gmail.com>
2025-02-12 20:58:17 +0100

    Move configuration to correct method.

--- Backend/Remora.Discord.API/Extensions/ServiceCollectionExtensions.cs
+++ Backend/Remora.Discord.API/Extensions/ServiceCollectionExtensions.cs
@@ -334,13 +334,6 @@ public static class ServiceCollectionExtensions
         options.AddDataObjectConverter<IGuildRoleUpdate, GuildRoleUpdate>();
         options.AddDataObjectConverter<IGuildRoleDelete, GuildRoleDelete>();
 
-        // TODO: move this
-        options.AddDataObjectConverter<IIncidentsData, IncidentsData>()
-            .WithPropertyName(i => i.DMsDisabledUntil, "dms_disabled_until")
-            .WithPropertyConverter(i => i.DMsDisabledUntil, new ISO8601DateTimeOffsetConverter())
-            .WithPropertyName(i => i.DMSpamDetectedAt, "dm_spam_detected_at")
-            .WithPropertyConverter(i => i.DMSpamDetectedAt, new ISO8601DateTimeOffsetConverter());
-
         // Guild scheduled events
         options.AddDataObjectConverter<IGuildScheduledEventCreate, GuildScheduledEventCreate>();
         options.AddDataObjectConverter<IGuildScheduledEventDelete, GuildScheduledEventDelete>();
@@ -739,6 +732,12 @@ public static class ServiceCollectionExtensions
             .WithPropertyName(o => o.RoleIDs, "role_ids");
 
         options.AddDataObjectConverter<IBulkBanResponse, BulkBanResponse>();
+
+        options.AddDataObjectConverter<IIncidentsData, IncidentsData>()
+            .WithPropertyName(i => i.DMsDisabledUntil, "dms_disabled_until")
+            .WithPropertyConverter(i => i.DMsDisabledUntil, new ISO8601DateTimeOffsetConverter())
+            .WithPropertyName(i => i.DMSpamDetectedAt, "dm_spam_detected_at")
+            .WithPropertyConverter(i => i.DMSpamDetectedAt, new ISO8601DateTimeOffsetConverter());
 
         return options;
     }
